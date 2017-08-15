bc0892e61172ecbc6778d9a9f81676ab953ce5ab
Sergey Fomenko <fomenko.personal@gmail.com>
2017-08-15 17:45:06 +0300

    update files

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const gulpIf = require("gulp-if");
 const browserSync = require("browser-sync").create();
 
 // Запуск `NODE_ENV=production gulp [задача]` приведет к сборке без sourcemaps
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", function() {
 
@@ -22,57 +22,61 @@ gulp.task("styles", function() {
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
     .pipe(postcss([
-          autoprefixer({browsers: [
-            "> 1%",
-            "last 2 versions",
-            "Firefox ESR"
-          ]})
-        ]))
+      autoprefixer({browsers: [
+        "> 1%",
+        "last 2 versions",
+        "Firefox ESR"
+      ]})
+    ]))
     .pipe(cssnano())
     .pipe(gulpIf(isDevelopment, sourcemaps.write()))
     .pipe(gulp.dest("build/css"));
-
 });
 
 gulp.task("clean", function() {
   return del("build");
 });
 
-gulp.task("assets", function() {
-  return gulp.src("src/**/*.html", {since: gulp.lastRun('assets')})
-      .pipe(newer("build"))
-      .pipe(gulp.dest("build"));
+gulp.task("markup", function() {
+  return gulp.src("src/**/*.html", {since: gulp.lastRun("markup")})
+    .pipe(newer("build"))
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("images", function() {
   return gulp.src("src/blocks/**/*.{png,jpg,svg}", {since: gulp.lastRun("images")})
-      .pipe(newer("build"))
-      .pipe(rename(function(path) {
-        path.dirname = "";
-        return path;
-      }))
-      .pipe(gulp.dest("build/img"));
+    .pipe(newer("build"))
+    .pipe(rename(function(path) {
+      path.dirname = "";
+      return path;
+    }))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("video", function() {
   return gulp.src("src/blocks/**/*.{mp4,flv,ogv,webm,avi}", {since: gulp.lastRun("video")})
-      .pipe(newer("build"))
-      .pipe(rename(function(path) {
-        path.dirname = "";
-        return path;
-      }))
-      .pipe(gulp.dest("build/video"));
+    .pipe(newer("build"))
+    .pipe(rename(function(path) {
+      path.dirname = "";
+      return path;
+    }))
+    .pipe(gulp.dest("build/video"));
+});
+
+gulp.task("assets", function() {
+  return gulp.src("src/assets/**/*.*")
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("build", gulp.series(
   "clean",
-  gulp.parallel("styles", "assets", "images", "video"))
+  gulp.parallel("styles", "markup", "images", "video", "assets"))
 );
 
 gulp.task("watch", function() {
   gulp.watch("src/blocks/**/*.*", gulp.series("styles"));
 
-  gulp.watch("src/**/*.html", gulp.series("assets"));
+  gulp.watch("src/**/*.html", gulp.series("markup"));
 
   gulp.watch("src/blocks/**/*.{png,jpg,svg}", gulp.series("images"));
 });
@@ -86,5 +90,5 @@ gulp.task("serve", function() {
 });
 
 gulp.task("dev",
-    gulp.series("build", gulp.parallel("watch", "serve"))
+  gulp.series("build", gulp.parallel("watch", "serve"))
 );
